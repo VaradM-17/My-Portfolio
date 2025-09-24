b3cc190cad98a17991e461bf21ce4af1fccd40e0
Varad Mule <varadmule17@gmail.com>
2025-09-24 22:51:33 +0530

    portfolio

--- my-portfolio/src/components/ContactForm.jsx
+++ my-portfolio/src/components/ContactForm.jsx
@@ -1,36 +1,62 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const ContactForm = () => {
+  const [state, handleSubmit] = useForm("manpyrbr");
+
+  if (state.succeeded) {
+    return <p>Thanks for reaching out! I'll get back to you soon.</p>;
+  }
+
   return (
-    <Form className="bg-light text-dark p-5 border shadow">
+    <Form
+      className="bg-light text-dark p-5 border shadow"
+      onSubmit={handleSubmit}
+    >
       <div className="row">
         <Form.Group className="mb-3 col-lg-6" controlId="firstName">
           <Form.Label className="w-100 text-start text-capitalize">
             First name
           </Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" name="firstName" required />
+          <ValidationError
+            prefix="First Name"
+            field="firstName"
+            errors={state.errors}
+          />
         </Form.Group>
         <Form.Group className="mb-3 col-lg-6" controlId="secondName">
           <Form.Label className="w-100 text-start text-capitalize">
             Second name
           </Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" name="secondName" required />
+          <ValidationError
+            prefix="Second Name"
+            field="secondName"
+            errors={state.errors}
+          />
         </Form.Group>
       </div>
       <Form.Group className="mb-3" controlId="emailAddress">
         <Form.Label className="w-100 text-start text-capitalize">
           Email address
         </Form.Label>
-        <Form.Control type="email" />
+        <Form.Control type="email" name="email" required />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
       </Form.Group>
       <Form.Group className="mb-4" controlId="textarea">
         <Form.Label className="w-100 text-start">Message</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control as="textarea" rows={3} name="message" required />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
       </Form.Group>
       <div className="text-center">
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" disabled={state.submitting}>
           Submit
         </Button>
       </div>
